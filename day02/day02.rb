3f79e53b4a24c3f78098407551fe8194283b569c
Meo Leung <misyleung@gmail.com>
2019-12-23 19:12:57 -0800

    [ADD] Day02 second half

--- day02/day02.rb
+++ day02/day02.rb
@@ -1,67 +1,134 @@
-def set_program_alarm_1202(int_arr)
-  #replace position 1 with the value 12
-  int_arr[1] = 12
-  # replace position 2 with the value 2
-  int_arr[2] = 2
-  return int_arr
-end
+class Intcode
 
-def opcode_helper(intcode_program, index, opcode)
-  # The three integers immediately after the opcode tell you these three positions
-# the first two indicate the positions from which you should read the input   values
-  first = intcode_program[index + 1]
-  second = intcode_program[index + 2]
-  # and the third indicates the position at which the output should be stored.
-  location = intcode_program[index + 3]
-  if intcode_program.size >= first || intcode_program.size >= second
-    if opcode == 1
-      # Opcode 1 adds together numbers read from two positions
-      result = intcode_program[first] + intcode_program[second]
-    elsif opcode == 2
-      # Opcode 2 multiples them together
-      result = intcode_program[first] * intcode_program[second]
+  def set_memory(values)
+    values.each.with_index do |value, index|
+      @memory[index] = value
+    end
+    @memory
+  end
+
+  # turns Intcode values into a Hash w/ @memory[0] = first_value
+  def initialize(values)
+    @values = values
+    @memory = Array.new()
+    set_memory(@values)
+  end
+
+  def reset_memory
+    @memory = Array.new()
+    set_memory(@values)
+  end
+
+  # this func replaces the value at address 1 to 12
+  # replace value at address 2 with the num 2
+  # each of the two input values will be between 0..99
+  def set_program_alarm(noun = 12, verb = 2)
+    @memory[1] = noun
+    @memory[2] = verb
+    return @memory
+  end
+
+  # brute force solution for part 2
+  def brute_force_calc_gravity_assist(goal = 19690720)
+    new_verb = -1
+    while (new_verb <= 100 && (addr0 = @memory[0]) != goal)
+      new_verb += 1 #must be between 0 .. 99
+      new_noun = 0
+      while (new_noun <= 100)
+        #must also be between 0 .. 99
+        puts "noun: #{new_noun}, verb: #{new_verb}"
+        set_program_alarm(new_noun, new_verb)
+        intcode_reader()
+        break if (addr0 = @memory[0]) == goal
+        puts "at #{addr0} : #{addr0 - 19690720} from goal: 19690720"
+        new_noun += 1
+        # reset the memory somehow
+        reset_memory
+      end
+    end
+    return new_noun, new_verb
+  end
+
+  # faster solution for part 2, but could do better
+  # say if diff is greater than 100, new_noun + 10
+  # want diff = 0
+  def faster_calc_gravity_assist(goal = 19690720, new_noun = 50, new_verb = 50)
+    set_program_alarm(new_noun, new_verb)
+    intcode_reader()
+    if (addr0 = @memory[0]) == goal
+      return new_noun, new_verb
     else
-      raise "Unknown opcode"
+      reset_memory
+      # puts "diff: #{addr0 - goal} @ #{new_noun}, #{new_verb}"
+      if (diff = addr0 - goal) < -100 and (new_noun + 1) <= 99
+        faster_calc_gravity_assist(goal, new_noun + 1, new_verb)
+      elsif diff > 100 and (new_noun - 1) >= 0
+        faster_calc_gravity_assist(goal, new_noun - 1, new_verb)
+      elsif diff > 100 and (new_verb + 1) <= 99
+        faster_calc_gravity_assist(goal, new_noun, new_verb + 1)
+      elsif diff <= 100 and (new_verb - 1) >= 0
+        faster_calc_gravity_assist(goal, new_noun, new_verb - 1)
+      end
     end
-    # and stores the result in a third position.
-    intcode_program[location] = result
-  else
-    # if the number is out of bounds, put error
-    raise "Error"
   end
-  return intcode_program
-end
 
-def intcode_reader(intcode_program)
-  next_index = 0
-  intcode_program.each.with_index do |intcode, index|
-    next_index == 0 ? next_index : next_index -= 1
-    next if next_index != 0
-    if intcode == 99
-      # 99 means that the program is finished and should immediately halt.
-      break
-    elsif intcode == 1 || intcode == 2
-      intcode_program = opcode_helper(intcode_program, index, intcode)
-      # move forward by stepping forward 4 positions.
-      next_index = 4
+  # The three integers immediately after the opcode tell you these three positions
+  # the first two indicate the positions from which you should read the input values
+  # and the third indicates the position at which the output should be stored.
+  def opcode_helper(index, opcode)
+    first = @memory[index + 1]
+    second = @memory[index + 2]
+    location = @memory[index + 3]
+    if @memory.size >= first && @memory.size >= second
+      if opcode == 1 #adds params
+        result = @memory[first] + @memory[second]
+      elsif opcode == 2 #multiplies params
+        result = @memory[first] * @memory[second]
+      else
+        raise "Unknown opcode"
+      end
+      @memory[location] = result
+    else # if the number is out of bounds, put error
+      raise "Error: memsize = #{@memory.size}, #{first} #{second}"
     end
+    return @memory
   end
-  p intcode_program
+
+  def intcode_reader
+    next_instruction = 0
+    @memory.each.with_index do |opcode, instruction_pointer|
+      next_instruction == 0 ? next_instruction : next_instruction -= 1
+      next if next_instruction != 0
+      case opcode
+      when 99 # program is finished; halt immediately
+        break
+      when 1..2
+        @memory = opcode_helper(instruction_pointer, opcode)
+        # move forward by stepping forward 4 positions.
+        next_instruction = 4
+      end
+    end
+    @memory
+  end
+
 end
 
 # Testing the code because i wanted to and it's kinda a good idea
-def test(int_arr, ans)
-  my_ans = intcode_reader(int_arr)
+def first_half_test(int_arr, ans)
+  arr = Intcode.new(int_arr)
+  my_ans = arr.intcode_reader
   puts my_ans == ans ? "OK" : "Failed: answer is not #{ans}"
 end
 
-test([1,9,10,3,2,3,11,0,99,30,40,50], [3500,9,10,70,2,3,11,0,99,30,40,50])
-test([1,0,0,0,99], [2,0,0,0,99])
-test([2,3,0,3,99], [2,3,0,6,99])
-test([2,4,4,5,99,0], [2,4,4,5,99,9801])
-test([1,1,1,4,99,5,6,0,99], [30,1,1,4,2,5,6,0,99])
+# first_half_test([1,9,10,3,2,3,11,0,99,30,40,50], [3500,9,10,70,2,3,11,0,99,30,40,50])
+# first_half_test([1,0,0,0,99], [2,0,0,0,99])
+# first_half_test([2,3,0,3,99], [2,3,0,6,99])
+# first_half_test([2,4,4,5,99,0], [2,4,4,5,99,9801])
+# first_half_test([1,1,1,4,99,5,6,0,99], [30,1,1,4,2,5,6,0,99])
 
-# day02 ans:
-intcode_reader(set_program_alarm_1202([1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,13,19,1,9,19,23,2,13,23,27,2,27,13,31,2,31,10,35,1,6,35,39,1,5,39,43,1,10,43,47,1,5,47,51,1,13,51,55,2,55,9,59,1,6,59,63,1,13,63,67,1,6,67,71,1,71,10,75,2,13,75,79,1,5,79,83,2,83,6,87,1,6,87,91,1,91,13,95,1,95,13,99,2,99,13,103,1,103,5,107,2,107,10,111,1,5,111,115,1,2,115,119,1,119,6,0,99,2,0,14,0]))
-
-#[3790689, 12, 2, 2, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 13, 60, 1, 9, 19, 63, 2, 13, 23, 315, 2, 27, 13, 1575, 2, 31, 10, 6300, 1, 6, 35, 6302, 1, 5, 39, 6303, 1, 10, 43, 6307, 1, 5, 47, 6308, 1, 13, 51, 6313, 2, 55, 9, 18939, 1, 6, 59, 18941, 1, 13, 63, 18946, 1, 6, 67, 18948, 1, 71, 10, 18952, 2, 13, 75, 94760, 1, 5, 79, 94761, 2, 83, 6, 189522, 1, 6, 87, 189524, 1, 91, 13, 189529, 1, 95, 13, 189534, 2, 99, 13, 947670, 1, 103, 5, 947671, 2, 107, 10, 3790684, 1, 5, 111, 3790685, 1, 2, 115, 3790687, 1, 119, 6, 0, 99, 2, 0, 14, 0]
+blep = Intcode.new([1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,13,19,1,9,19,23,2,13,23,27,2,27,13,31,2,31,10,35,1,6,35,39,1,5,39,43,1,10,43,47,1,5,47,51,1,13,51,55,2,55,9,59,1,6,59,63,1,13,63,67,1,6,67,71,1,71,10,75,2,13,75,79,1,5,79,83,2,83,6,87,1,6,87,91,1,91,13,95,1,95,13,99,2,99,13,103,1,103,5,107,2,107,10,111,1,5,111,115,1,2,115,119,1,119,6,0,99,2,0,14,0])
+blep.set_program_alarm
+puts "first half: #{blep.intcode_reader.first}" #3790689
+# part 2
+noun, verb =  blep.faster_calc_gravity_assist
+puts "second half: #{100 * noun + verb}" #6533
